21257a2873184ed390d900907d2386248fe68869
Christian Medel <cmedelahumada@protonmail.com>
2023-06-25 12:34:01 -0400

    Add status field for action interfaces

--- app/src/main/java/dev/elbullazul/copepod/api/common/actions/Boosts.kt
+++ app/src/main/java/dev/elbullazul/copepod/api/common/actions/Boosts.kt
@@ -2,4 +2,5 @@ package dev.elbullazul.copepod.api.common.actions
 
 interface Boosts {
     var boosts: Int
+    var boosted: Boolean
 }
--- app/src/main/java/dev/elbullazul/copepod/api/common/actions/Favorites.kt
+++ app/src/main/java/dev/elbullazul/copepod/api/common/actions/Favorites.kt
@@ -2,4 +2,5 @@ package dev.elbullazul.copepod.api.common.actions
 
 interface Favorites {
     var favorites: Int
+    var favorited: Boolean
 }
