5b6e0f59a36a2a36155e115c0da6ed738f27a257
Christian Medel <cmedelahumada@protonmail.com>
2023-06-27 16:45:25 -0400

    Create blog view

---
+++ app/src/main/java/dev/elbullazul/copepod/ui/fragments/views/BlogListView.kt
@@ -0,0 +1,9 @@
+package dev.elbullazul.copepod.ui.fragments.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BlogListView() {
+    Text("In progress")
+}
